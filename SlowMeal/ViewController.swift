b16f531a0c26a0325e0b2db3809ea55866dd8fa1
miokato <miokato@me.com>
2021-01-11 15:55:26 +0900

    Initial Commit

---
+++ SlowMeal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SlowMeal
+//
+//  Created by mio kato on 2021/01/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
